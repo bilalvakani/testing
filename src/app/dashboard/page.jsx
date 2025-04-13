71cc5641c676140c21ca2a8480a99030fe4b4b0b
Bilal-dev <143352356+bilalvakani@users.noreply.github.com>
2025-04-13 13:51:24 -0700

    add CardPageVisits and CardSocialTraffic components to Dashboard; update layout to include new charts

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,8 @@
 'use client'; 
 import CardBarChart from "@/components/cards/CardBarChart";
 import CardLineChart from "@/components/cards/CardLineChart";
+import CardPageVisits from "@/components/cards/CardPageVisits";
+import CardSocialTraffic from "@/components/cards/CardSocialTraffic";
 import HeaderStats from "@/components/headers/headerStat";
 import AdminNavbar from "@/components/navbars/adminNavbar";
 import React from "react";
@@ -28,14 +30,14 @@ function Dashboard() {
           <CardBarChart />
         </div>
           </div>
-          {/* <div className="flex flex-wrap mt-4">
+          <div className="flex flex-wrap mt-4">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
           <CardPageVisits />
         </div>
         <div className="w-full xl:w-4/12 px-4">
           <CardSocialTraffic />
         </div>
-      </div> */}
+      </div>
         </div>
       </div>
     </>
@@ -43,3 +45,42 @@ function Dashboard() {
 }
 
 export default Dashboard;
+
+
+// import React from "react";
+
+// // components
+
+// import CardLineChart from "components/Cards/CardLineChart.js";
+// import CardBarChart from "components/Cards/CardBarChart.js";
+// import CardPageVisits from "components/Cards/CardPageVisits.js";
+// import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
+
+// // layout for page
+
+// import Admin from "layouts/Admin.js";
+
+// export default function Dashboard() {
+//   return (
+//     <>
+//       <div className="flex flex-wrap">
+//         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+//           <CardLineChart />
+//         </div>
+//         <div className="w-full xl:w-4/12 px-4">
+//           <CardBarChart />
+//         </div>
+//       </div>
+//       <div className="flex flex-wrap mt-4">
+//         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+//           <CardPageVisits />
+//         </div>
+//         <div className="w-full xl:w-4/12 px-4">
+//           <CardSocialTraffic />
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
+
+// Dashboard.layout = Admin;
