55541c4e87206b204ba27e3dfd2e9bc5ebfdff88
Bilal-dev <143352356+bilalvakani@users.noreply.github.com>
2025-04-13 13:25:11 -0700

    add CardBarChart and CardLineChart components; update Dashboard to include charts

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,3 +1,6 @@
+'use client'; 
+import CardBarChart from "@/components/cards/CardBarChart";
+import CardLineChart from "@/components/cards/CardLineChart";
 import HeaderStats from "@/components/headers/headerStat";
 import AdminNavbar from "@/components/navbars/adminNavbar";
 import React from "react";
@@ -17,12 +20,13 @@ function Dashboard() {
         <HeaderStats />
         <div className="px-4 md:px-10 mx-auto w-full -m-24">
           <div className="flex flex-wrap">
-            <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
+            <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4 ">
               {/* <CardLineChart /> */}
+              <CardLineChart/>
             </div>
-            {/* <div className="w-full xl:w-4/12 px-4">
+            <div className="w-full xl:w-4/12 px-4">
           <CardBarChart />
-        </div> */}
+        </div>
           </div>
           {/* <div className="flex flex-wrap mt-4">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
